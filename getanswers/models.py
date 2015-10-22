b2e56cd0e7408a277cce74688dcd27458d7156eb
Adam Hartz <ahartz1@gmail.com>
2015-10-22 15:04:18 -0400

    Add first pass at models

--- getanswers/models.py
+++ getanswers/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    points = models.IntegerField(null=True)
+    date_created = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    qtext = models.TextField()
+    asker = models.ForeignKey(Profile)
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    answerer = models.ForeignKey(Profile)
+    atext = models.TextField()
+    score = models.IntegerField()
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return self.atext
