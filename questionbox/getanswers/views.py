072ea5b615c23bb0ff3cda3d33f6a21e9e1d4701
Jonathan Frederick <jrfreder@gmail.com>
2015-10-24 11:15:49 -0400

    directing registration to homepage

--- questionbox/getanswers/views.py
+++ questionbox/getanswers/views.py
@@ -18,7 +18,7 @@ from .forms import QuestionForm, AnswerForm
 class UserCreateView(FormView):
     template_name = 'register.html'
     form_class = UserCreationForm
-    success_url = '/register'  # TODO: change to question creation?
+    success_url = '/'  # TODO: change to question creation?
 
     def form_valid(self, form):
         new_user = form.save()
